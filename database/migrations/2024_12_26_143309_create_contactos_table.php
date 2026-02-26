0db49c8951b15d09de0b1574cc55ab740f4e9b03
Julián Isaza C <89994334+Julianisazac@users.noreply.github.com>
2026-02-25 20:41:21 -0500

    Changes in config/database: configure the database correctly Change and add migrations in the database file.

--- database/migrations/2024_12_26_143309_create_contactos_table.php
+++ database/migrations/2024_12_26_143309_create_contactos_table.php
@@ -18,8 +18,10 @@ return new class extends Migration
             $table->string('telefono')->nullable(); // Teléfono (opcional)
             $table->string('direccion')->nullable(); // Dirección (opcional)
             $table->text('notas')->nullable(); // Notas (opcional)
-            $table->unsignedBigInteger('entidad_id'); // Relación con entidades
-            $table->foreign('entidad_id')->references('id')->on('entidades')->onDelete('set null');
+            $table->foreignId('entidad_id')
+                ->nullable()
+                ->constrained('entidades')
+                ->nullOnDelete();
             $table->date('fecha_nacimiento')->nullable(); // Fecha de nacimiento (opcional)
             $table->unsignedBigInteger('creado_por')->nullable(); // Relación con usuarios (opcional)
             $table->timestamps(); // created_at y updated_at
